485ed25c332386234d13d0ee03f5fd9e812fa2fe
Nguyễn Minh Khang <110124083@st.tvu.edu.vn>
2024-12-06 22:18:06 +0700

    SelectionSort

---
+++ SelectionSort.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void selectionSort(int a[], int n){
+	int pos;
+	for (int i=0;i<n-1;i++){
+		pos=i;
+		for (int j=i+1;j<n;j++){
+			if (a[j]<a[pos]){
+				pos=j;
+			}
+		}	
+		int t=a[pos];
+		a[pos]=a[i];
+		a[i]=t;
+	}	
+}
+int main(){
+	int n;scanf("%d",&n);int a[n];
+	for (int i=0;i<n;i++) scanf("%d",&a[i]);
+	selectionSort(a,n);
+	for (int i=0;i<n;i++) printf("%d ",a[i]);
+	return 0;
+}
